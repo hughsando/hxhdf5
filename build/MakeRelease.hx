7d38480d9845ea3d2c1e07d3f52f464ce9a1b3d3
Hugh Sanderson <gamehaxe@gmail.com>
2018-04-03 15:10:19 +0800

    Add zip phase to release build

--- build/MakeRelease.hx
+++ build/MakeRelease.hx
@@ -2,6 +2,8 @@ package build;
 
 import sys.FileSystem;
 import sys.io.File;
+import haxe.zip.Entry;
+import haxe.zip.Writer;
 
 class MakeRelease
 {
@@ -64,23 +66,64 @@ class MakeRelease
       }
    }
 
+   static function zipRecurse(src:String, dest:String, entries:List<Entry>)
+   {
+      if (!FileSystem.exists(src))
+         throw 'File does not exist : $src';
+
+      if (FileSystem.isDirectory(src))
+      {
+         for(file in FileSystem.readDirectory(src))
+           zipRecurse(src+"/"+file, dest+"/"+file, entries);
+      }
+      else
+      {
+         Sys.print('$dest -> ');
+
+          var bytes = sys.io.File.getBytes(src);
+          var zipped = haxe.zip.Compress.run(bytes,9);
+          zipped = zipped.sub(2,zipped.length-6);
+          if (zipped.length > bytes.length*0.9)
+             zipped = null;
+          entries.add( {
+             fileName : dest,
+             fileSize : bytes.length,
+             fileTime : Date.now(),
+             compressed : zipped!=null,
+             dataSize : zipped==null ? 0 : zipped.length,
+             data : zipped==null ? bytes : zipped,
+             crc32 : haxe.crypto.Crc32.make(bytes),
+             extraFields : new List()
+          } );
+         Sys.println( zipped==null ? bytes.length : zipped.length );
+      }
+   }
+
+
+
    public static function main()
    {
       var version:String = haxe.Json.parse( File.getContent("haxelib.json") ).version;
-      var releaseDir = "hxhdf5-" + version ;
-      deleteRecurse(releaseDir);
-      FileSystem.createDirectory(releaseDir);
+      var releaseDir = "hxhdf5-" + version;
+      var releaseName = releaseDir + ".zip";
+      if (FileSystem.exists(releaseName))
+         FileSystem.deleteFile(releaseName);
+
+      var entries:List<Entry> = new List();
 
       for(file in files)
       {
-         if (!FileSystem.isDirectory(file))
-         {
-            var path = file.split("/");
-            path.pop();
-            var dir = path.join("/");
-            FileSystem.createDirectory(releaseDir + "/" +dir);
-         }
-         copyRecurse(file, releaseDir + "/" + file);
+         zipRecurse(file, releaseDir + "/" + file,entries);
       }
+
+      var bytesOutput = new haxe.io.BytesOutput();
+      var writer = new Writer(bytesOutput);
+      writer.write(entries);
+      // Grab the zipped file from the output stream
+      var zipfileBytes = bytesOutput.getBytes();
+      // Save the zipped file to disc
+      var file = File.write(releaseName, true);
+      file.write(zipfileBytes);
+      file.close();
    }
 }
